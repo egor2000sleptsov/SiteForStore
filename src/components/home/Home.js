457ae8ba18000f6519490f5ed60df2daf82d98ba
umbalar <71994054+umbalar@users.noreply.github.com>
2021-01-07 20:26:24 +1000

    Test
    Testing

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Carousel from "./Carousel/Carousel";
+import TestF from "./TestF";
 class Home extends Component {
 
     render() {
         return (
             <div>
                 <Carousel/>
+                <TestF/>
             </div>
         );
     }
