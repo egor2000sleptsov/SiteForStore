4864b75e5eb5349d43836a5c29398577db9f6864
Egor <egor2000sleptsov@gmail.com>
2021-01-07 20:43:01 +1000

    delete unusable component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import Carousel from "./Carousel/Carousel";
-import TestF from "./TestF";
 class Home extends Component {
 
     render() {
         return (
             <div>
                 <Carousel/>
-                <TestF/>
             </div>
         );
     }
--- src/components/home/TestF.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function TestF(props) {
-    return (
-        <div><h1>Hello World!</h1></div>
-    );
-}
-
-export default TestF;
